93b48f076c7fd953edccf673ae137a8754ead8b3
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-14 05:05:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors
+from matplotlib.ticker import PercentFormatter
+
+# Create a random number generator with a fixed seed for reproducibility
+rng = np.random.default_rng(19680801)
